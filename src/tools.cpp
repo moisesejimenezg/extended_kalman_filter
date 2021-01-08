175884010aa9ef8aca3ef90a2402dfb3a68a7daf
Moises Jimenez <moisesejimenezg@outlook.com>
2021-01-08 23:19:38 +0100

    Make tools free floating functions

--- src/tools.cpp
+++ src/tools.cpp
@@ -4,21 +4,19 @@
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
-
-Tools::Tools() {}
-
-Tools::~Tools() {}
-
-VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+namespace Tools
+{
+VectorXd CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
    * TODO: Calculate the RMSE here.
    */
 }
 
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
+MatrixXd CalculateJacobian(const VectorXd& x_state) {
   /**
    * TODO:
    * Calculate a Jacobian here.
    */
+}
 }
--- src/tools.h
+++ src/tools.h
@@ -4,18 +4,7 @@
 #include <vector>
 #include "Eigen/Dense"
 
-class Tools {
- public:
-  /**
-   * Constructor.
-   */
-  Tools();
-
-  /**
-   * Destructor.
-   */
-  virtual ~Tools();
-
+namespace Tools {
   /**
    * A helper method to calculate RMSE.
    */
@@ -26,7 +15,6 @@ class Tools {
    * A helper method to calculate Jacobians.
    */
   Eigen::MatrixXd CalculateJacobian(const Eigen::VectorXd& x_state);
-
-};
+}
 
 #endif  // TOOLS_H_
