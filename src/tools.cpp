99f021448a6d4c7105fd1352321b90a00f55cb52
Moises Jimenez <moisesejimenezg@outlook.com>
2021-01-08 23:26:21 +0100

    Apply clang-format

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,22 +1,25 @@
 #include "tools.h"
+
 #include <iostream>
 
-using Eigen::VectorXd;
 using Eigen::MatrixXd;
+using Eigen::VectorXd;
 using std::vector;
 namespace Tools
 {
 VectorXd CalculateRMSE(const vector<VectorXd> &estimations,
-                              const vector<VectorXd> &ground_truth) {
-  /**
-   * TODO: Calculate the RMSE here.
-   */
+                       const vector<VectorXd> &ground_truth)
+{
+    /**
+     * TODO: Calculate the RMSE here.
+     */
 }
 
-MatrixXd CalculateJacobian(const VectorXd& x_state) {
-  /**
-   * TODO:
-   * Calculate a Jacobian here.
-   */
+MatrixXd CalculateJacobian(const VectorXd &x_state)
+{
+    /**
+     * TODO:
+     * Calculate a Jacobian here.
+     */
 }
-}
+}  // namespace Tools
--- src/tools.h
+++ src/tools.h
@@ -2,19 +2,21 @@
 #define TOOLS_H_
 
 #include <vector>
+
 #include "Eigen/Dense"
 
-namespace Tools {
-  /**
-   * A helper method to calculate RMSE.
-   */
-  Eigen::VectorXd CalculateRMSE(const std::vector<Eigen::VectorXd> &estimations, 
-                                const std::vector<Eigen::VectorXd> &ground_truth);
+namespace Tools
+{
+/**
+ * A helper method to calculate RMSE.
+ */
+Eigen::VectorXd CalculateRMSE(const std::vector<Eigen::VectorXd> &estimations,
+                              const std::vector<Eigen::VectorXd> &ground_truth);
 
-  /**
-   * A helper method to calculate Jacobians.
-   */
-  Eigen::MatrixXd CalculateJacobian(const Eigen::VectorXd& x_state);
-}
+/**
+ * A helper method to calculate Jacobians.
+ */
+Eigen::MatrixXd CalculateJacobian(const Eigen::VectorXd &x_state);
+}  // namespace Tools
 
 #endif  // TOOLS_H_
